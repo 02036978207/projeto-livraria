b8c9c898e10541014c1bddba0bf3b7c3a2b435de
camila veiga <camilaveiga.ham@gmail.com>
2024-03-06 14:51:31 -0400

    crud

--- js/index.js
+++ js/index.js
@@ -2,11 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.post('/thiago', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+app.get('/deleteId', (req, res) => {
+  res.send('Delete!')
+})
+
+app.get('/update', (req, res) => {
+  res.send('update!')
+})
+
+
+
+app.get('/read', (req, res) => {
+  res.send('read!')
+})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
