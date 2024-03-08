dce0da2826da5a27789025819a210c4f0411f6d1
camila veiga <camilaveiga.ham@gmail.com>
2024-03-08 15:28:47 -0400

    Criete

--- js/item.js
+++ js/item.js
@@ -2,77 +2,33 @@ const port = 3000
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
 
+app.use(bodyParser.urlencoded({ extended: true}))// tratamento de dados para mostrar o objeto body
 
 app.get('/produtos', (req, res) => { //essa função me tras a lista de todos os produtos
     res.send(bancoDeDados.getProdutos())
 })
 
-app.post('/produtos/:id', (req, res) => { //essa função me tras um unico id de produtos
+app.get('/produtos/:id', (req, res) => { //essa função me tras um unico id de produtos
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos/:nome/:descricao/:preco/:dataCadastro/:dataAtualizada', (req, res, next) => {
+app.post('/produtos', (req, res, next) => {//cria e salva o produto 
     const produto = bancoDeDados.salvarProduto({
-        nome: req.params.nome,
-        descricao: req.params.descricao,
-        preco: req.params.preco,
-        dataCadastro: req.params.dataCadastro,
-        dataAtualizada: req.params.dataAtualizada
+        nome: req.body.nome,
+        descricao: req.body.descricao,
+        preco: req.body.preco,
+        dataCadastro: req.body.dataCadastro,
+        dataAtualizada: req.body.dataAtualizada
     })
     res.send(produto)
 })
-
-
-
-
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
   
-
-// app.post('/produtos/:id/:nome/:descricao/:preco/:dataCadastro/:dataAtualizada', (req, res) => {
-
-
-//     bancoDeDados.splice(0, 3)  
-//     bancoDeDados[2] =   {
-//         id: 1,
-//         nome: "Senhor dos Anéis",
-//         descricao: 'história',
-//         preco: 12.99,
-//         dataCadastro: '08/06/2024',
-//         dataAtualizada: '08/06/2024'
-//     }
-
-//     bancoDeDados[3] =   {
-//         id: 1,
-//         nome: "Senhor dos Anéis",
-//         descricao: 'história',
-//         preco: 12.99,
-//         dataCadastro: '08/06/2024',
-//         dataAtualizada: '08/06/2024'
-//     }
-
-
-//     //Método para adicionar um array no final 
-//     bancoDeDados.push({
-//         id: 7,
-//         nome: "Senhor dos Anéis",
-//         descricao: 'história',
-//         preco: 12.99,
-//         dataCadastro: '08/06/2024',
-//         dataAtualizada: '08/06/2024'
-//     });
-
-
-
-//     console.log(bancoDeDados)
-    
-//     res.send(bancoDeDados)
-
-// })
-
 
